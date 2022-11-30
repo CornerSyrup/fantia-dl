8ca9490b5717f34decce69045270e2cb099c05fb
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-11-30 15:37:57 +0000

    feat: :sparkles: Create download post command

--- main.go
+++ main.go
@@ -1,5 +1,66 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strconv"
+)
 
+func main() {
+	session := flag.String("session", "", "Session ID which has been logged in to Fantia")
+	dir := flag.String("dir", "", "Directory to store the downloaded contents")
+	postId := flag.Int("post", 0, "Post ID")
+
+	flag.Parse()
+
+	if len(*session) < 1 {
+		panic("session ID cannot be empty")
+	}
+
+	if len(*dir) == 0 {
+		*dir = filepath.Dir("")
+	}
+	if path, err := filepath.Abs(*dir); err != nil {
+		panic(err.Error())
+	} else {
+		*dir = path
+	}
+	if stat, err := os.Stat(*dir); err != nil {
+		panic(err.Error())
+	} else if !stat.IsDir() {
+		panic(fmt.Sprintf("dir %s is not a directory", *dir))
+	}
+
+	if *postId == 0 {
+		panic("post id is empty")
+	}
+
+	agent := NewAgent(*session)
+	api, err := FetchPost(agent, *postId)
+	if err != nil {
+		panic(err)
+	}
+
+	postRoot := filepath.Join(*dir, fmt.Sprintf("%d_%s", api.Post.Fanclub.ID, api.Post.Fanclub.FanclubName), fmt.Sprintf("%d_%s", api.Post.ID, api.Post.Title))
+	os.MkdirAll(postRoot, fs.ModeDir)
+
+	for _, content := range api.Post.PostContents {
+		root := filepath.Join(postRoot, fmt.Sprintf("%d_%s", content.Plan.Price, content.Title))
+		os.Mkdir(root, fs.ModeDir)
+
+		switch content.Category {
+		case "file":
+			if content.DownloadURI == "" {
+				continue
+			}
+			DownloadContent(agent, root, BaseUrl+content.DownloadURI, content.Title)
+		case "photo_gellery":
+			for _, photo := range content.PostContentPhotos {
+				go DownloadContent(agent, root, photo.URL.Original, strconv.Itoa(photo.ID))
+			}
+		}
+	}
 }
