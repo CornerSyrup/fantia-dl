0d75ab8b7286182a9d24f99e533533fd0768c8d1
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-12-11 01:55:50 +0000

    fix: :adhesive_bandage: Fix default usage message
    backnumber sub command is missed

--- main.go
+++ main.go
@@ -12,6 +12,11 @@ func main() {
 	var cmd command.Command
 	var fs *flag.FlagSet
 
+	if len(os.Args[1:]) < 1 {
+		fmt.Fprintf(os.Stderr, "Usage:\n\n\tfantia-dl <post|backnumber> [arguments]\n")
+		os.Exit(0)
+	}
+
 	switch os.Args[1] {
 	case "post":
 		fs = flag.NewFlagSet("post", flag.ExitOnError)
@@ -20,7 +25,7 @@ func main() {
 		fs = flag.NewFlagSet("backnumber", flag.ExitOnError)
 		cmd = command.NewBacknumberCommand(fs)
 	default:
-		fmt.Fprintf(os.Stderr, "Usage:\n\n\tfantia-dl post [arguments]\n")
+		fmt.Fprintf(os.Stderr, "Usage:\n\n\tfantia-dl <post|backnumber> [arguments]\n")
 		os.Exit(0)
 	}
 
